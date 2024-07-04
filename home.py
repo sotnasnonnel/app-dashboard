3b6296bb42a459227ffdd8287dbe3a0ffd520bd3
sotnasnonnel <90616389+sotnasnonnel@users.noreply.github.com>
2024-07-04 11:45:41 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,11 @@
 import streamlit as st  
 
+# Adicione a função configure_page aqui
+def configure_page():
+    st.set_page_config(layout="wide", page_title="Dashboard", page_icon=":bar_chart:")
+
+# Chame a função configure_page imediatamente após defini-la
+configure_page()
 
 st.sidebar.markdown('Desenvolvido por Lennon')
 
